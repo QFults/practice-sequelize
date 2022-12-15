fdc521ae22e7c28bc159de28e82c0a695127b298
Quinton Fults <quinton.fults.work@gmail.com>
2022-12-15 13:57:12 -0800

    code

--- models/User.js
+++ models/User.js
@@ -7,10 +7,23 @@ User.init(
   {
     username: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true 
+      }
     },
     age: {
       type: DataTypes.INTEGER,
+      allowNull: false
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    }
   },
   {
     sequelize,
